532b9c1f20d31379ce9276c75cf8d8031eeba8a1
Akshat Tuknait <akshattuknait123@gmail.com>
2022-08-25 20:10:56 +0530

    adding spoj solutions too

---
+++ Spoj/Horrible_Queries.cpp
@@ -0,0 +1,238 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+const long long mod = 1e9+7;
+//const long long mod =1000000007;
+//const long long mod = 998244353;
+const long long int special_prime = 982451653;
+using namespace std;
+
+#define test int t; cin>>t; while(t--)
+
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopr(i,a,b) for(int i=a;i>=b;i--)
+#define loops(i,a,b,step) for(int i=a;i<b;i+=step)
+#define looprs(i,a,b,step) for(int i=a;i>=b;i-=step)
+
+#define ll long long int
+#define P pair
+#define PLL pair<long long, long long>
+#define PII pair<int, int>
+#define PUU pair<unsigned long long int, unsigned long long int>
+#define L list
+#define V vector
+#define Q queue
+#define D deque
+#define ST set
+#define MS multiset
+#define mp make_pair
+#define pb emplace_back
+#define pf push_front
+#define MM multimap
+#define F first
+#define S second
+#define TH third
+#define IT iterator
+#define RIT reverse_iterator
+#define FAST ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define FILE_READ_IN freopen("input.txt","r",stdin);
+#define FILE_READ_OUT freopen("output.txt","w",stdout);
+#define all(a) a.begin(),a.end()
+#define ld long double
+#define sz(x) (int)((x).size())
+#define square(x) ((x)*(x))
+#define cube(x) ((x)*(x)*(x))
+#define distance(a,b,c,d) ((a-c)*1LL*(a-c)+(b-d)*1LL*(b-d))
+#define range(i,x,y) ((x <= i) && (i <= y))
+#define SUM(a) accumulate(a.begin(),a.end(),0LL)
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define lb lower_bound
+#define ub upper_bound
+#define REV reverse
+#define mset(a,b) memset(a,b,sizeof(a))
+const long long N = 1e6 + 100;
+ int pr[N]; void preSieve(){for(int i = 2; i < N; ++i){if(!pr[i])for(int j = i; j < N; j+=i)pr[j]=i;}}
+ ll inv[N]; void preModInv(){inv[0]=0;inv[1]=1;for(int i = 2; i < N; ++i)inv[i] = mod-mod/i*inv[mod%i]%mod;}
+
+ ll ifact[N]; void preiFact(){ifact[0]=1; ifact[1] = 1; for(int i = 2; i < N; ++i)ifact[i]=ifact[i-1]*inv[i]%mod;}
+
+ ll fact[N];
+void preFact(){fact[0] = 1,fact[1] = 1; for(int i = 2; i < N; ++i)fact[i]=fact[i-1]*i%mod;}
+// Randomization
+ 
+ 
+ 
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+// pair operation
+template<class T, class U>istream& operator>>(istream& in, pair<T,U> &rhs){in >> rhs.first;in >> rhs.second;return in;}
+template<class T, class U>ostream& operator<<(ostream& out,const pair<T,U> &rhs){out << rhs.first;out << " ";out << rhs.second;return out;}
+template<class T, class U>pair<T,U> operator+(pair<T,U> &a, pair<T,U> &b){return pair<T,U>(a.first+b.first,a.second+b.second);}
+template<class T, class U>pair<T,U> operator-(pair<T,U> &a, pair<T,U> &b){return pair<T,U>(a.first-b.first,a.second-b.second);}
+// Linear STL
+// VECTOR
+template<class T>istream& operator>>(istream& in, vector<T> &a){for(auto &i: a)cin >> i;return in;}
+template<class T>ostream& operator<<(ostream& out, const vector<T> &a){for(auto &i: a)cout << i << " ";return out;}
+// SET
+template<class T>ostream& operator<<(ostream& out, const set<T> &a){for(auto &i: a)cout << i << " ";return out;}
+template<class T>ostream& operator<<(ostream& out, const unordered_set<T> &a){for(auto &i: a)cout << i << " ";return out;}
+template<class T>ostream& operator<<(ostream& out, const multiset<T> &a){for(auto &i: a)cout << i << " ";return out;}
+// MAP
+template<class T,class U>ostream& operator<<(ostream& out, const map<T,U> &a){for(auto &i: a)cout << "(" << i.first << ", " << i.second << ")\n";return out;}
+template<class T,class U>ostream& operator<<(ostream& out, const unordered_map<T,U> &a){for(auto &i: a)cout << "(" << i.first << ", " << i.second << ")\n";return out;}
+ 
+using namespace __gnu_pbds;
+
+#define Min(a, b) ((a < b) ? a : b)
+#define Max(a, b) ((a < b) ? b : a)
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+#define inf 1000000000000000 
+#define INF3 1e18      
+const int XN = 1e5 + 3;
+// main code here
+const int MX = 200001; //check the limits, dummy
+const int NX = 2750132;
+const int mxX=1e5+7;
+const int nx = 18;
+const int M1 = 1e9+9;
+const int INF = 1e9+7;
+void init_code()
+{
+FAST;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+     freopen("output.txt","w",stdout);
+#endif
+
+}
+const int rn= 3e5+5;
+ll n,m,k;
+V<ll>adj[MX],adj2[MX];
+ll a[MX],b[MX],dp1[MX],child[MX];
+ll X,Y;
+int check=0;
+template <typename num_t> 
+struct segtree {
+  int n, depth;
+  vector<num_t> tree, lazy;
+ 
+  void init(int s, long long* arr) {
+    n = s;
+    tree = vector<num_t>(4 * s, 0);
+    lazy = vector<num_t>(4 * s, 0);
+    init(0, 0, n - 1, arr);
+  }
+ 
+  num_t init(int i, int l, int r, long long* arr) {
+    if (l == r) return tree[i] = num_t(arr[l], l);
+ 
+    int mid = (l + r) / 2;
+    num_t a = init(2 * i + 1, l, mid, arr),
+          b = init(2 * i + 2, mid + 1, r, arr);
+    return tree[i] = a.op(b);
+  }
+ 
+  void update(int l, int r, num_t v) {
+    update(0, 0, n - 1, l, r, v);
+  }
+ 
+  num_t update(int i, int tl, int tr, int ql, int qr, num_t v) {
+    eval_lazy(i, tl, tr);
+ 
+    if (ql <= tl && tr <= qr) {
+      lazy[i] = lazy[i].val + v.val;
+      eval_lazy(i, tl, tr);
+      return tree[i];
+    }
+    if (tl > tr || tr < ql || qr < tl) return tree[i];
+    if (tl == tr) return tree[i];
+ 
+    int mid = (tl + tr) / 2;
+    num_t a = update(2 * i + 1, tl, mid, ql, qr, v),
+          b = update(2 * i + 2, mid + 1, tr, ql, qr, v);
+    return tree[i] = a.op(b);
+  }
+ 
+  num_t query(int l, int r) {
+    return query(0, 0, n-1, l, r);
+  }
+ 
+  num_t query(int i, int tl, int tr, int ql, int qr) {
+    eval_lazy(i, tl, tr);
+    
+    if (ql <= tl && tr <= qr) return tree[i];
+    if (tl > tr || tr < ql || qr < tl) return num_t::null_v;
+ 
+    int mid = (tl + tr) / 2;
+    num_t a = query(2 * i + 1, tl, mid, ql, qr),
+          b = query(2 * i + 2, mid + 1, tr, ql, qr);
+    return a.op(b);
+  }
+ 
+  /* varies by implementation */
+  /* this one is for range additions */
+  void eval_lazy(int i, int l, int r) {
+    /* special part */
+    tree[i] = tree[i].lazy_op(lazy[i], (r - l + 1));
+    if (l != r) {
+      lazy[i * 2 + 1] = lazy[i].val + lazy[i * 2 + 1].val;
+      lazy[i * 2 + 2] = lazy[i].val + lazy[i * 2 + 2].val;
+    }
+    /* end special part */
+ 
+    lazy[i] = num_t();
+  }
+};
+struct sum_t {
+  long long val;
+  int ind;
+  static const long long null_v = 0;
+ 
+  sum_t(): val(0) {}
+  sum_t(long long v): val(v) {}
+  sum_t(long long v, int i): val(v), ind(i) {};
+ 
+  sum_t op(sum_t& other) {
+    return sum_t(val + other.val,ind);
+  }
+  
+  sum_t lazy_op(sum_t& v, int size) {
+    return sum_t(val + v.val * size,ind);
+  }
+};
+int main()
+{
+init_code();
+cout<<setprecision(12)<<fixed;
+int tes=1;
+//compPrimes(rn);
+cin>>tes;
+while(tes--)
+{  
+   segtree<sum_t>st;
+   cin>>n>>m;
+   loop(i,0,n)
+   {
+    a[i]=0;
+   }
+   st.init(n,a);
+   loop(i,0,m)
+   {
+         int Q,x,y; cin>>Q>>x>>y;
+         x--; y--;
+         if(Q==0)
+         {
+            ll val; cin>>val;
+            st.update(x,y,val);
+         }else
+         {  
+               ll ans=st.query(x,y).val;
+               cout<<ans<<endl;
+         }
+
+   }  
+
+}
+return 0;
+}
